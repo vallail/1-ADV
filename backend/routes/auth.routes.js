dada5f7cbf2d43886470ceb9e737d57230d9b45a
Vallail-dev <vallail.naqvi@v2solutions.com>
2024-12-13 11:39:35 +0530

    twitter-app

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  getMe,
+  login,
+  logout,
+  signup,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.get("/me", protectedRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/logout", logout);
+router.post("/logout", logout);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { urlencoded } from "express";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+
 const app = express();
+
 const PORT = process.env.PORT || 5000;
+
+//to parse the req.body (json)
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is Running on port : ${PORT}`);
   connectMongoDB();
