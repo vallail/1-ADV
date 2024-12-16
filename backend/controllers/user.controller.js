9d91ea025acb1bc4db55532b373c5ddad1c73d58
Vallail-dev <vallail.naqvi@v2solutions.com>
2024-12-16 14:25:07 +0530

    updated notification schema

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
 export const getUserProfile = async (req, res) => {
@@ -39,6 +40,13 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
 
       //send a notification to user
+      const newNotification = new Notification({
+        type: "follow",
+        from: req.user._id,
+        to: userToModify._id,
+      });
+
+      await newNotification.save();
       res.status(200).json({ message: "User followed Successfully" });
     }
   } catch (error) {
